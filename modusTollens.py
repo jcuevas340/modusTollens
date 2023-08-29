1ca872ec5d4173661b5adf75d34cf6932cb57607
Juan Cuevas <78194459+jcuevas340@users.noreply.github.com>
2023-08-28 19:43:07 -0600

    Create modusTollens.py

---
+++ modusTollens.py
@@ -0,0 +1,8 @@
+# Afirmaciones
+afirmacion1 = True # Si el vaso esta mojado, entonces lo lavaron
+afirmacion2 = False # El vaso no esta mojado
+
+# Modus tollens
+if not afirmacion1:
+  conclusion = No lavaron el vaso
+  print(conclusion)
